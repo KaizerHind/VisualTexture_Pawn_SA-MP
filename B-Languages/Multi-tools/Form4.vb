04409cb8743224661ec9d3bd5abc084c6d13d407
KaizerHind <igamesproducctions@gmail.com>
2019-07-10 01:26:49 -0500

    Update Form4.vb

--- B-Languages/Multi-tools/Form4.vb
+++ B-Languages/Multi-tools/Form4.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form4
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        Close()
+        Me.Close()
     End Sub
 
 End Class
