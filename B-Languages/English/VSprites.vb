faf273c213c06c534beca7770e529ae84d196be4
KaizerHind <igamesproducctions@gmail.com>
2019-07-13 22:14:31 -0500

    Update VSprites.vb

--- B-Languages/English/VSprites.vb
+++ B-Languages/English/VSprites.vb
@@ -151,4 +151,9 @@ Public Class VSprites
         frmMain.Show()
         Me.Hide()
     End Sub
+
+    Private Sub PawnWikiToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PawnWikiToolStripMenuItem.Click
+        VSWiki.Show()
+        Me.Hide()
+    End Sub
 End Class
